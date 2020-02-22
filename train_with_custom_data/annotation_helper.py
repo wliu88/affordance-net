7dbabb11d71d58b845f14c9419e4649ed77699f2
Weiyu Liu <wliu88@gatech.edu>
2020-02-22 15:19:18 -0500

    Added notes for naming files

--- train_with_custom_data/annotation_helper.py
+++ train_with_custom_data/annotation_helper.py
@@ -5,6 +5,7 @@ import os
 from xml.dom.minidom import parseString
 import numpy as np
 import cPickle
+from random import shuffle
 
 
 def bbox_json_to_xml(taskname, input_dir, output_dir):
@@ -72,7 +73,7 @@ def mask_npy_to_sm(input_dir, output_dir):
             data = data.astype(np.uint8)
             img_name = filename.split(".")[0]
             # only support 1 object per image for now
-            sm_filename = "{}_{}_segmask.sm".format(img_name, 0)
+            sm_filename = "{}_{}_segmask.sm".format(img_name, 1)
 
             print(sm_filename)
             ## write each mask to sm file
@@ -83,12 +84,14 @@ def mask_npy_to_sm(input_dir, output_dir):
 def write_train_test_files(output_dir):
     with open(os.path.join(output_dir, "train.txt"), "w") as fh:
         ids = list(range(1, 37)) + list(range(55, 91))
-        img_names = ["frame{:04d}".format(id) for id in ids]
+        shuffle(ids)
+        img_names = ["{}".format(id) for id in ids]
         fh.write("\n".join(img_names))
 
     with open(os.path.join(output_dir, "test.txt"), "w") as fh:
         ids = list(range(1, 37)) + list(range(55, 91))
-        img_names = ["frame{:04d}".format(id) for id in ids]
+        shuffle(ids)
+        img_names = ["{}".format(id) for id in ids]
         fh.write("\n".join(img_names))
 
 
@@ -100,4 +103,9 @@ if __name__ == "__main__":
     # mask_npy_to_sm(input_dir="/home/weiyu/Research/labelme/labelme/examples/semantic_segmentation/custom_data/masks/SegmentationClass",
     #                output_dir="/home/weiyu/Research/labelme/labelme/examples/semantic_segmentation/custom_data/masks/SegmentationMask")
 
-    write_train_test_files("/home/weiyu/Research/AffordanceNet/affordance-net/data/VOCdevkit2012/VOC2012/ImageSets/Main")
+    # write_train_test_files("/home/weiyu/Research/AffordanceNet/affordance-net/data/VOCdevkit2012/VOC2012/ImageSets/Main")
+
+    # input_dir = "/home/weiyu/Research/labelme/labelme/examples/semantic_segmentation/custom_data/masks/SegmentationClass"
+    # for filename in os.listdir(input_dir):
+    #     new_filename = str(int(filename[5:].split(".")[0])) + "." + filename.split(".")[1]
+    #     os.rename(os.path.join(input_dir, filename), os.path.join(input_dir, new_filename))
